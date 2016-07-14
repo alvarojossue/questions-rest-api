220256b421500328bedd4e4af0f2954cc869e1b5
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-07-13 22:05:45 -0400

    Create instance methods for updating and voting answers

--- models.js
+++ models.js
@@ -4,6 +4,16 @@ var mongoose = require("mongoose");
 
 var = Schema. mongoose.Schema;
 
+var sortAnswers = function(a,b){
+	// - negative a before b
+	// 0 no change
+	// + positie a after b
+	if(a.votes === b.votes){
+		return a.updatedAt - b.updatedAt
+	}
+	return b.votes - a.votes;
+}
+
 var AnswerSchema = new Schema({ //Creates AnswerSchema (child). Declare first the parent's child. 
 	text: String,
 	createdAt: {type: Date, default: Date.now},
@@ -11,11 +21,32 @@ var AnswerSchema = new Schema({ //Creates AnswerSchema (child). Declare first th
 	votes: {type: Number, default: 0}
 });
 
+AnswerSchema.method.("update", function(updates, callback){ //Update method (instance method)
+	Object.assign(this, updates, {updatedAt: new Date()})
+	this.parent().save(callback);
+});
+
+AnswerSchema.method.("vote", function(updates, callback){ // Vote method
+	if(vote === "up"){
+		this.votes += 1;
+	} else {
+		this.votes -= 1;
+	}
+});
+
+
 var QuestionSchema = new Schema({ // Creates Question Schema (parent)
 	text: String,
 	createdAt: {type: Date, default: Date.now},
 	answers: [AnswerSchema] // Sets the association between questions and its answers. 
 }); 
+
+//pre save callback
+
+QuestionSchema.pre("save", function(next){
+	this.answers.sort(sortAnswers);
+	next();
+})
 
 var Question = mongoose.model("Question", QuestionSchema);
 
